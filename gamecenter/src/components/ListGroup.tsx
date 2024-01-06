967aa7183c2c94cdf03162e19842cb7690f3d188
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-06 17:19:00 +0330

    using fragment

--- gamecenter/src/components/ListGroup.tsx
+++ gamecenter/src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    // This is another way to use Fragment from react lib
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
   );
 }
 
