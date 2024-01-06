8216aa32326699588d4e4d00b4820429ff9c9f68
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-06 16:29:05 +0330

    rendering simple component into react using jsx

---
+++ gamecenter/src/Message.tsx
@@ -0,0 +1,10 @@
+
+// Use Pascal Casing
+function Message() {
+    let name = 'Pouya';
+    // This is not HTML. it is JSX (javascript xml) 
+    let res = name == '' ? "World" : name 
+    return <h1>Hello {res}</h1>; 
+}
+
+export default Message;
