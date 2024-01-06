35a5f619dfef92cc09a6a2c3bc80b9401c187a1d
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-06 17:37:22 +0330

    adding conditional rendering

--- gamecenter/src/components/ListGroup.tsx
+++ gamecenter/src/components/ListGroup.tsx
@@ -1,10 +1,17 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  //   using function is good for passing parameters but const is sufficient for this case
+  //   const getMessage = () => {
+  //     return items.length === 0 ? <p>No Item Found</p> : null;
+  //   };
 
   return (
     // This is another way to use Fragment from react lib
     <>
       <h1>List</h1>
+      {/* {getMessage()} */}
+      {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
           // use unique key for each item to interact with the item later
