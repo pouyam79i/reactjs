7d8ba988b19980c4fd4c30aad3273bcd52989f0e
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-06 20:27:45 +0330

    using children prop

--- gamecenter/src/App.tsx
+++ gamecenter/src/App.tsx
@@ -1,18 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
-// App component
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <b>World</b>
+      </Alert>
     </div>
   );
 }
