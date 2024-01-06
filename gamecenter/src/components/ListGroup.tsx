82bd8449c0f25e89a4710a78d969fe092752b1f9
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-06 17:46:25 +0330

    handling events

--- gamecenter/src/components/ListGroup.tsx
+++ gamecenter/src/components/ListGroup.tsx
@@ -1,21 +1,18 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  //   using function is good for passing parameters but const is sufficient for this case
-  //   const getMessage = () => {
-  //     return items.length === 0 ? <p>No Item Found</p> : null;
-  //   };
+  // define handles outside of jsx for better code understanding
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
-    // This is another way to use Fragment from react lib
     <>
       <h1>List</h1>
-      {/* {getMessage()} */}
       {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          // use unique key for each item to interact with the item later
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
