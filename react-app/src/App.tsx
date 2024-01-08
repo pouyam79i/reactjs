75b066e0c7d956eb9b110252ba9f694d016a6dac
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-08 12:22:51 +0330

    a litte more with concepts

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,32 @@
-import Like from "./components/Like";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <Like
-        comment="Hello"
-        onClick={(item: string) => {
-          console.log("Clicked with: ", item);
-        }}
-      />
-    </div>
-  );
+  // Concepts:
+  // 1- always build your components as pure function: same x in same y out f(x)=y
+  // 2- do not build deeply nested objects cause it is hard to update
+  const [info, setInfo] = useState({
+    name: "Pouya",
+    address: {
+      street: "some street name",
+      postalCode: 54841,
+    },
+  });
+
+  // as you can see you have to treat objects as they are immutable
+  // so updating nested ones are harder.
+  setInfo({ ...info, address: { ...info.address, postalCode: 123412 } });
+
+  // working with array:
+  const [array, setArray] = useState(["Ali", "Muhammad"]);
+  // Add
+  setArray([...array, "Joe"]);
+  // Remove
+  setArray(array.filter((item) => item != "Ali"));
+  // update
+  setArray(array.map((item) => (item === "Joe" ? "Reza" : item)));
+
+  // As you can see there is too many re-renders so this is NOT GOOD!
+  return <div></div>;
 }
 
 // Always export components
