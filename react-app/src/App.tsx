21f49f3ae65bffa0d494c7a57752a335bb626191
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-08 13:59:32 +0330

    adding update with more complex example

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,32 +3,42 @@ import Button from "./components/Button";
 import produce from "immer";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    players: {
-      name: "Pouya",
-    },
+  const [list, setList] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, name: "item 1", quantity: 1 },
+      { id: 2, name: "item 2", quantity: 1 },
+    ],
   });
 
   const handle = () => {
-    // to update objects you can use spread '...' notation to copy all fields before updating any other field
-    // setGame({ ...game, players: { ...game.players, name: "Reza" } });
+    // Normal way:
+    // setList({
+    //   ...list,
+    //   items: list.items.map((item) =>
+    //     item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+    //   ),
+    // });
 
-    // or use immer
-    setGame(
+    // Use Immer
+    setList(
       produce((draft) => {
-        draft.players.name = "Reza";
+        // find the item
+        const item = draft.items.find((item) => item.id === 1);
+        // if found change the wanted field
+        if (item) item.quantity += 1;
       })
     );
   };
 
   return (
     <div>
-      {
-        <p>
-          {game.id} - players: {game.players.name}
+      {<p key={-1}>Discount List Items number: {list.items.length}</p>}
+      {list.items.map((item) => (
+        <p key={item.id}>
+          {item.name} - quantity: {item.quantity}
         </p>
-      }
+      ))}
       <Button setOnClick={handle} />
     </div>
   );
