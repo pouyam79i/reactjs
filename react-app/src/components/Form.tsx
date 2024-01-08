3dea68b74195ea51374c3233a1684d15089e44bb
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-08 20:56:53 +0330

    applying validation to form data

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,8 +1,20 @@
 import { FieldValues, useForm } from "react-hook-form";
 
-const Form = () => {
-  const { register, handleSubmit } = useForm();
+// define form data to make useForm understand form data shape
+interface FormData {
+  name: string;
+  age: number;
+}
 
+const Form = () => {
+  // deconstructing useForm props
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  console.log(errors);
   const submitData = (data: FieldValues) => {
     console.log(data);
   };
@@ -15,11 +27,18 @@ const Form = () => {
             Name
           </label>
           <input
-            {...register("name")}
+            {...register("name", {
+              required: true,
+              minLength: 3,
+              maxLength: 50,
+            })}
             id="name"
             type="text"
             className="form-control"
           />
+          {/* using optional chains '?' in 'errors.name?.type' makes it easier*/}
+          {errors.name?.type === "required" && <p>Name field is required!</p>}
+          {errors.name?.type === "minLength" && <p>Name min length is 3!</p>}
           <div className="mb-3">
             <label htmlFor="age" className="form-label">
               Age
