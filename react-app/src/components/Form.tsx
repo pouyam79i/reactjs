1aaf2994a72f6a75491beb7dbd4bb33477d33a5b
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-08 21:39:19 +0330

    is the form valid?

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 // define schemas with 'zod' for validation rules
 const schema = z.object({
-  name: z.string().min(3).max(3),
+  name: z.string().min(3).max(40),
   age: z
     .number({ invalid_type_error: "Age field is required!" })
     .min(18, { message: "The person must be at least 18 years old!" })
@@ -18,10 +18,9 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
-  console.log(errors);
   const submitData = (data: FieldValues) => {
     console.log(data);
   };
@@ -51,7 +50,11 @@ const Form = () => {
               className="form-control"
             />
             {errors.age && <p>{errors.age.message}</p>}
-            <button className="btn btn-primary" type="submit">
+            <button
+              disabled={!isValid}
+              className="btn btn-primary"
+              type="submit"
+            >
               Submit
             </button>
           </div>
