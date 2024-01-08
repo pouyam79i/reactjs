dfd52acbab5016be94ca8eb927734866007f7d51
Pouya <pouyamohammadyirbu@gmail.com>
2024-01-08 19:49:24 +0330

    using state hook

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,19 +1,15 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
-  // always use 'null' to initial ref hook
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  //   you can use objects to gather form data and send this object to a server
-  const person = {
+  // using state hook in this case makes it easier but re-rendering with every change might
+  // cause performance issues so in complex pages use the ref hook.
+  const [person, setPerson] = useState({
     name: "",
-    age: 0,
-  };
+    age: "",
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
     console.log(person);
   };
 
@@ -24,13 +20,26 @@ const Form = () => {
           <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input ref={nameRef} id="name" type="text" className="form-control" />
+          <input
+            //   using controlled values - so it will not get out of sync
+            value={person.name}
+            onChange={(event) => {
+              setPerson({ ...person, name: event.target.value });
+            }}
+            id="name"
+            type="text"
+            className="form-control"
+          />
           <div className="mb-3">
             <label htmlFor="age" className="form-label">
               Age
             </label>
             <input
-              ref={ageRef}
+              //   using controlled values - so it will not get out of sync
+              value={person.age}
+              onChange={(event) => {
+                setPerson({ ...person, age: parseInt(event.target.value) });
+              }}
               id="age"
               type="number"
               className="form-control"
